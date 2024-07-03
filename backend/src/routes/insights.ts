02b85a41772ddb13a5c2c9d3f5a3f15c04f5b692
Peris Muniu <122822923+perismuniu@users.noreply.github.com>
2024-07-03 23:20:48 +0300

    Update insights.ts

--- backend/src/routes/insights.ts
+++ backend/src/routes/insights.ts
@@ -1,14 +1,1 @@
-import { Router } from "express";
-import { isAdmin, isAuthenticated } from "../utils/auth.middleware";
-import { insight } from "../utils/pipeline";
 
-const inRouter = Router()
-
-
-inRouter.get("/insights", isAuthenticated, isAdmin, async (req: any, res: any) => {
-    console.log(insight)
-    res.status(200).json({ insight })
-})
-
-
-export default inRouter
