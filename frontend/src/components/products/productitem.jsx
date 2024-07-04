3f0951167b8ba03b6893bc870b9afdb31ca8346f
Peris Muniu <122822923+perismuniu@users.noreply.github.com>
2024-07-04 14:22:57 +0300

    homepage - shop now button update

--- frontend/src/components/products/productitem.jsx
+++ frontend/src/components/products/productitem.jsx
@@ -8,7 +8,7 @@ const ProductItem = ({ id, image, name, price, sizes, colors, reviews }) => {
           <img src={image} alt="Product" className="w-full h-[50%] mb-4 rounded-lg" />
           <h3 className="text-lg font-semibold text-center">{name}</h3>
           <p className="text-center">Price: KSh {price}</p>
-          <button className="px-4 py-2 mt-4 text-white bg-pink rounded-full" onClick={() => navigate(`/products/:${id}`)}>SHOP NOW</button>
+          <button className="px-4 py-2 mt-4 text-white bg-light-pink rounded-full font-bold" onClick={() => navigate(`/products/:${id}`)}>SHOP NOW</button>
         </div>
     )
 };
